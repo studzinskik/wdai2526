d2a1fcfa22f530cb95bc9343969d173673d35bf0
Kamil Studziński <studzinski.kamil@outlook.com>
2026-01-08 15:38:02 +0100

    lab4

--- lab4/services/books/index.js
+++ lab4/services/books/index.js
@@ -13,6 +13,11 @@ app.use("/api/books", booksRouter);
 
 async function start(){
     await sequelize.sync();
+
+    // await Book.bulkCreate([
+    //     { title: "Pan Tadeusz", author: "Adam Mickiewicz", year: 1834 },
+    //     { title: "Lalka", author: "Bolesław Prus", year: 1890 }
+    // ]);
     
     app.listen(PORT, ()=> console.log(`Books service listening ${PORT}`));
 }
